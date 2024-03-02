97c1c6fdb065cda3622e08277bd8e084c70ff614
Matvii Kharchenko <matviy.kharchenko@gmail.com>
2024-03-02 11:36:20 +0100

    react-router added

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,15 @@
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+const browserRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <RouterProvider router={browserRouter} />
+)
