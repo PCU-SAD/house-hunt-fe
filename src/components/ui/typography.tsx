72c15dfbdacbecb303f5566eb18a409a85e9313e
Matvii Kharchenko <matviy.kharchenko@gmail.com>
2024-04-13 15:37:59 +0200

    base path router

--- src/components/ui/typography.tsx
+++ src/components/ui/typography.tsx
@@ -10,7 +10,7 @@ const typographyVariants = cva('leading-7 m-0', {
       h2: 'scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight',
       h3: 'scroll-m-20 text-2xl font-semibold tracking-tight',
       h4: 'scroll-m-20 text-xl font-semibold tracking-tight',
-      p: 'leading-7',
+      p: 'leading-6',
       muted: 'text-sm text-muted-foreground'
     }
   },
