b245954e662028ce060e8a3942576aeb028701f4
Matvii Kharchenko <matviy.kharchenko@gmail.com>
2024-04-13 15:16:51 +0200

    error page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,7 +2,8 @@ import Loading from '@/pages/loading/Loading'
 import App from './App'
 import Login from './auth/login/LoginPage'
 import Signup from './auth/signup/SignupPage'
+import ErrorPage from './error/ErrorPage'
 import Home from './home/Home'
 import NotFound from './not-found/NotFound'
 
-export { App, Home, Loading, Login, NotFound, Signup }
+export { App, ErrorPage, Home, Loading, Login, NotFound, Signup }
