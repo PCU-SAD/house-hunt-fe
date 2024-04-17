acae737aaced4b1ef34b52e335b83056d7f8707c
Matvii Kharchenko <matviy.kharchenko@gmail.com>
2024-04-17 20:44:59 +0200

    deleted router.tsx

--- src/router.tsx
+++
@@ -1,6 +0,0 @@
-import { ErrorPage, NotFound } from '@/pages'
-import { QueryClient } from '@tanstack/react-query'
-import { createHashHistory, createRouter } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
-
-
