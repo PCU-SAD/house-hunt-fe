a14236522a3dc0303aae499a20fbd8f6e3832534
Matvii Kharchenko <matviy.kharchenko@gmail.com>
2024-04-22 18:30:52 +0200

    reafactored tabs

--- src/pages/auth/signup/SignupPage.tsx
+++ src/pages/auth/signup/SignupPage.tsx
@@ -1,8 +1,20 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
-import OwnerTabForm from '@/pages/auth/components/tabs/OwnerTabForm/OwnerTabForm'
-import TenantTabForm from '@/pages/auth/components/tabs/TenantTabForm/TenantTabForm'
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import SignupForm from '@/pages/auth/components/SignupForm/SignupForm'
+import { useState } from 'react'
+
+export type UserTypeTab = 'tenant' | 'owner'
 
 function SignupPage() {
+  const [userTypeTab, setUserTypeTab] = useState<UserTypeTab>('tenant')
+
+  function handleOwnerTabClick() {
+    setUserTypeTab('owner')
+  }
+
+  function handleTenantTabClick() {
+    setUserTypeTab('tenant')
+  }
+
   return (
     <div className="flex h-screen flex-col sm:items-center">
       <div className="mt-12 px-4">
@@ -10,17 +22,17 @@ function SignupPage() {
           defaultValue="tenant"
           className="mx-auto w-full min-w-[330px] max-w-[400px]">
           <TabsList className="grid w-full grid-cols-2">
-            <TabsTrigger value="tenant">Tenant</TabsTrigger>
-            <TabsTrigger value="owner">Owner</TabsTrigger>
+            <TabsTrigger value="tenant" onClick={handleTenantTabClick}>
+              Tenant
+            </TabsTrigger>
+            <TabsTrigger value="owner" onClick={handleOwnerTabClick}>
+              Owner
+            </TabsTrigger>
           </TabsList>
 
-          <TabsContent value="tenant">
-            <TenantTabForm />
-          </TabsContent>
-
-          <TabsContent value="owner">
-            <OwnerTabForm />
-          </TabsContent>
+          <div className="mt-2">
+            <SignupForm userType={userTypeTab} />
+          </div>
         </Tabs>
       </div>
     </div>
