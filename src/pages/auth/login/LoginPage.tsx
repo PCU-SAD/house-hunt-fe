1ea7a5d9c233cd4ac1848e424fea3f098696d1a1
Matvii Kharchenko <matviy.kharchenko@gmail.com>
2024-04-30 17:37:21 +0200

    drawer

--- src/pages/auth/login/LoginPage.tsx
+++ src/pages/auth/login/LoginPage.tsx
@@ -1,7 +1,25 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger
+} from '@/components/ui/sheet'
 import LoginForm from '@/pages/auth/login/LoginForm/LoginForm'
 
 function LoginPage() {
-  return <LoginForm />
+  return (
+    <Sheet defaultOpen>
+      <SheetTrigger>Open</SheetTrigger>
+      <SheetContent className="flex w-full flex-col items-center sm:max-w-fit">
+        <SheetHeader className="mt-6">
+          <SheetTitle>Sign in</SheetTitle>
+        </SheetHeader>
+
+        <LoginForm />
+      </SheetContent>
+    </Sheet>
+  )
 }
 
 export default LoginPage
