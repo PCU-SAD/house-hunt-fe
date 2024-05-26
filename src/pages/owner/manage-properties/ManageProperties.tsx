6b92b535511184978549adc1b838a2f4ac8667e3
Matvii Kharchenko <matviy.kharchenko@gmail.com>
2024-05-26 15:14:55 +0200

    block creating request to non verified landlords

--- src/pages/owner/manage-properties/ManageProperties.tsx
+++ src/pages/owner/manage-properties/ManageProperties.tsx
@@ -1,13 +1,19 @@
 import { Container, Layout } from '@/components/common'
-import { buttonVariants } from '@/components/ui/button'
+import { Button, buttonVariants } from '@/components/ui/button'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from '@/components/ui/tooltip'
 import { Typography } from '@/components/ui/typography'
 import { cn } from '@/lib/utils'
 import ManagePropertiesTable from '@/pages/owner/manage-properties/ManagePropertiesTable/ManagePropertiesTable'
 import RequestedPropertiesTable from '@/pages/owner/manage-properties/RequestedPropertiesTable/RequestedPropertiesTable'
 import { useAuthContext } from '@/providers/AuthProvider/AuthProvider'
 import { Link } from '@tanstack/react-router'
-import { Clock, HomeIcon, PlusIcon } from 'lucide-react'
+import { Clock, HomeIcon, Info, PlusIcon } from 'lucide-react'
 import { FC, useState } from 'react'
 
 type ManagePropertiesProps = {}
@@ -15,6 +21,8 @@ type ManagePropertiesProps = {}
 type PropertiesTableTab = 'properties' | 'requests'
 
 const ManageProperties: FC<ManagePropertiesProps> = () => {
+  const auth = useAuthContext()
+  const isVerified = auth.user.status === 'VERIFIED'
   const email = useAuthContext().user?.email ?? ''
   const [activeTab, setActiveTab] = useState<PropertiesTableTab>('requests')
 
@@ -30,19 +38,41 @@ const ManageProperties: FC<ManagePropertiesProps> = () => {
             Manage Properties
           </Typography>
 
-          <Link
-            to="/manage-properties/add-new"
-            className={cn(
-              buttonVariants({
-                size: 'noSize',
-                className: 'px-2 py-2',
-                variant: 'outline'
-              }),
-              'border-none bg-green-600 text-white hover:bg-green-500 hover:text-white'
-            )}>
-            <PlusIcon className="h-4 w-4" />
-            <span>Add new request</span>
-          </Link>
+          {isVerified && (
+            <Link
+              to="/manage-properties/add-new"
+              className={cn(
+                buttonVariants({
+                  size: 'noSize',
+                  className: 'px-2 py-2',
+                  variant: 'outline'
+                }),
+                'border-none bg-green-600 text-white hover:bg-green-500 hover:text-white'
+              )}>
+              <PlusIcon className="h-4 w-4" />
+              <span>Add new request</span>
+            </Link>
+          )}
+
+          {!isVerified && (
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  <Button variant="outline" disabled size="sm">
+                    <span>Add new request</span>
+
+                    <Info className="h-4 w-4" />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent className="w-[200px]">
+                  <p>
+                    You need to verify your identity in order to create a
+                    property request
+                  </p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          )}
         </div>
 
         <Tabs value={activeTab} className="mt-6 w-full">
