0802fe79d5e401c6ebd92d58c505f5c462d777ed
Matvii Kharchenko <matviy.kharchenko@gmail.com>
2024-05-26 23:46:19 +0200

    Mobile menu fix

--- src/components/common/Layout/Layout.tsx
+++ src/components/common/Layout/Layout.tsx
@@ -3,6 +3,7 @@ import { FC, ReactNode } from 'react'
 import Footer from '@/components/common/Layout/Footer/Footer'
 
 import AuthDrawer from '@/components/common/Layout/Header/AuthDrawer/AuthDrawer'
+import MobileMenu from '@/components/common/Layout/Header/MobileMenu'
 import { Link } from '@tanstack/react-router'
 import Container from './Container'
 import Header from './Header/Header'
@@ -17,6 +18,9 @@ const Layout: FC<LayoutProps> = ({ children }) => {
     <main className="flex min-h-screen flex-col">
       <header className="border-b bg-gray-900 text-white">
         <Container className="flex h-[70px] items-center justify-between">
+          <div className="md:hidden">
+            <MobileMenu />
+          </div>
           <Link to="/">
             <img src={Logo} className="h-10" />
           </Link>
